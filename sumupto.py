48179daa208e30fca3f43c4005d2e01e487bde19
PAUL CAULFIELD <SESA87346@se.com>
2019-03-07 20:29:49 +0000

    Created solution 1 - ask user to input a positive integer

---
+++ sumupto.py
@@ -0,0 +1,4 @@
+# Paul Caulfield, 2019
+# My program asks the user to input any positive integer and outputs the sum of all numbers between one and that number.
+
+x = int(input("Please enter a positive integer: "))
