78041202b91c20c95d76a08ff9ce2162cf572352
PAUL CAULFIELD <SESA87346@se.com>
2019-03-07 20:41:29 +0000

    Added code to calculate sum of all numbers between 1 and number input by user

--- sumupto.py
+++ sumupto.py
@@ -2,3 +2,13 @@
 # My program asks the user to input any positive integer and outputs the sum of all numbers between one and that number.
 
 x = int(input("Please enter a positive integer: "))
+
+
+
+total = 0
+
+while x > 0:
+    total = total + x
+    x = x - 1
+
+print(total)
