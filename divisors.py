276549b9a8a8d074fbe3203294c14f56b7d44d03
PAUL CAULFIELD <SESA87346@se.com>
2019-03-09 10:13:59 +0000

    added comments to explain program functions

--- divisors.py
+++ divisors.py
@@ -1,9 +1,16 @@
 # Paul Caulfield, 2019
 # My program prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
 
-for i in range(1000, 2000):
+for i in range(1000, 10000):
+    # i is the variable: i is contained in a range of numbers between 1000 and 10000
         if (i % 6 == 0) and (i % 12 != 0):
+            # The argument is true if i can be divided by 6 but not 12
+                # condition 1 % 6 == 0 is true when i can be divided by 6 with no remainder, 
+                # condition 1 % 12 != 0 is true when i divided by 12 gives remainder,
+                # both conditions must be met for argument to be true, so I joined them together using "and" function, as 
             print(i)
+            # print i if argument is true
         else:
             continue
+            # if argument is false, that is where i fails to meet both conditions, then continue with the next iteration of the loop
                            
