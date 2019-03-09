a4907b09090d536c7ab97eb29afbdbaab1831783
PAUL CAULFIELD <SESA87346@se.com>
2019-03-09 10:15:50 +0000

    correct typo in comment

--- divisors.py
+++ divisors.py
@@ -6,7 +6,7 @@ for i in range(1000, 10000):
         if (i % 6 == 0) and (i % 12 != 0):
             # The argument is true if i can be divided by 6 but not 12
                 # condition 1 % 6 == 0 is true when i can be divided by 6 with no remainder, 
-                # condition 1 % 12 != 0 is true when i divided by 12 gives remainder,
+                # condition 1 % 12 != 0 is true when i divided by 12 gives a remainder,
                 # both conditions must be met for argument to be true, so I joined them together using "and" function, as 
             print(i)
             # print i if argument is true
