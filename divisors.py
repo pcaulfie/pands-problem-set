ced02e250561c8888bc16dba21bc8e0b5efa84ee
PAUL CAULFIELD <SESA87346@se.com>
2019-03-09 07:51:25 +0000

    Added code to check if number was divisible by 6

--- divisors.py
+++ divisors.py
@@ -2,7 +2,9 @@
 # My program prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
 
 for i in range(1000, 2000):
-    if i % 6 == 0:
-        if i % == 12
-        break
-    else: print(i)
+        if i % 6 == 0: 
+        ## and i % 12 != 0:
+            print(i)
+        else:
+            print(i, "is not divisible by 6")
+    
