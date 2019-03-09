7f6a794cfbdc0e732d1d3a6822cfdde5c38322c7
PAUL CAULFIELD <SESA87346@se.com>
2019-03-09 09:55:28 +0000

    tidied up the code to join 2 arguments together using and function and figured out how to stop printing numbers which failed to meet the condition by using the continue function to continue with next iteration of the loop

--- divisors.py
+++ divisors.py
@@ -2,11 +2,8 @@
 # My program prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
 
 for i in range(1000, 2000):
-        if i % 6 == 0: 
-            if i % 12 != 0:
-                print(i)
-            else:
-                print(i, "is divisible by 6 and 12")
+        if (i % 6 == 0) and (i % 12 != 0):
+            print(i)
         else:
-            print(i, "is not divisible by 6")
-    
+            continue
+                           
