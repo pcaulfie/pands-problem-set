977ed0c65739b3cb5d5befdd52e71c61ecd97f06
PAUL CAULFIELD <SESA87346@se.com>
2019-03-09 10:39:45 +0000

    Created first draft of solution to problem 4.

---
+++ collatz.py
@@ -0,0 +1,19 @@
+# Paul Caulfield, 2019
+# My program asks the user to input any positive integer and 
+    # outputs the successive values of the following calculation:
+#       # At each step calculate the next value by taking the current value and ;
+            # if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
+            # The program ends if the current value is one.
+
+x = int(input("Please enter a positive integer: "))
+# x is the value input by the user. 
+# INT will return an integer object constructed from a number or string x input by the user. https://docs.python.org/3/library/functions.html?highlight=int#int
+while x >= 1:
+# The while loop executes as long as the condition (here: x >= 1) remains true.
+    if i % 2 == 0:
+        x  = x // 2
+        print(x)
+    else 
+        if i % 2 != 0:
+            x = (x * 3) + 1
+            print(x)
