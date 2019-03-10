461bfdcf2a1fcd7546d235c9f036bf32a8d7a2fb
PAUL CAULFIELD <SESA87346@se.com>
2019-03-10 07:40:02 +0000

    reused code with some changes from https://stackoverflow.com/a/17645629

--- secondstring.py
+++ secondstring.py
@@ -1,9 +1,6 @@
 # Paul Caulfield, 2019
 # My program takes a user input string and outputs every second word.
 
-My_list = []
-
 x=input("Please enter a sentance: ")
-# x is the value input by the user. 
-list.append(x)
-print(len(My_list))
+for word in x.split()[::2]:
+    print(word)
