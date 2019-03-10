b835bb23ac526a55e89a748d16829f0a905745ae
PAUL CAULFIELD <SESA87346@se.com>
2019-03-10 23:02:41 +0000

    amended the input object to float data type

--- squareroot.py
+++ squareroot.py
@@ -1,6 +1,7 @@
 # Paul Caulfield, 2019
 # My program takes a positive floating point number as input and outputs an approximation of its square root
 
-x = decimal(input("Please enter a positive number: "))
+x = float(input("Please enter a positive number: "))
 # x is the value input by the user.
 # the input is converted in decimal so the program can perform floating point arithmetic. https://docs.python.org/3.7/tutorial/stdlib2.html#decimal-floating-point-arithmetic
+print(x)
