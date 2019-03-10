d016324c54d61ce2ad4691970b448119f2f2440f
PAUL CAULFIELD <SESA87346@se.com>
2019-03-10 07:20:11 +0000

    Create a list of words, from sentance entered by user

--- secondstring.py
+++ secondstring.py
@@ -1,5 +1,9 @@
 # Paul Caulfield, 2019
 # My program takes a user input string and outputs every second word.
 
-x = ""(input("Please enter a sentance: "))
+My_list = []
+
+x=input("Please enter a sentance: ")
 # x is the value input by the user. 
+list.append(x)
+print(len(My_list))
