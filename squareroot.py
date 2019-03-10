0271d966ab1a326030ad182208857991eee527e9
PAUL CAULFIELD <SESA87346@se.com>
2019-03-10 23:37:35 +0000

    corrected error where original number x was being rounded at time of output

--- squareroot.py
+++ squareroot.py
@@ -5,5 +5,9 @@ x = float(input("Please enter a positive number: "))
 # x is the value input by the user, converted into decimat using float data type, ref https://docs.python.org/3.3/library/stdtypes.html 
 import math
 # https://www.geeksforgeeks.org/python-math-function-sqrt/
-print(round(math.sqrt(x),1))
+z = x + 0
+
+y = (round(math.sqrt(z),1))
 # sqrt() function is an inbuilt function in Python programming language that returns the square root of any number.
+print("The square root of ",x," is approx.",y,".")
+# prints the solution
