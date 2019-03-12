d9106d97faeaf3b34c15dbed23c2cae5e1e0d19e
PAUL CAULFIELD <SESA87346@se.com>
2019-03-12 21:03:35 +0000

    Formatting the date & time so it prints in the correct format

--- todaysdatetime.py
+++ todaysdatetime.py
@@ -1,7 +1,8 @@
 # Paul Caulfield, 2019
 # My program that outputs today’s date and time in the format “Monday, January 10th 2019 at 1:15pm"
 
-from datetime import datetime
+import datetime
 now = datetime.datetime.now()
-print ("Current date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+print (now.strftime("%A, %B %D %Y")) 
+# print (now.strftime("%Y-%m-%d %H:%M:%S")) 
