37adbaca3581e41443c6be623478aab7b03fe9a6
PAUL CAULFIELD <SESA87346@se.com>
2019-03-17 08:00:21 +0000

    defined x axis values in the range [0,4]

--- displayplot.py
+++ displayplot.py
@@ -1,2 +1,7 @@
 # Paul Caulfield, 2019
-# My program displays a plot of the functions x, x2 and 2x in the range [0, 4].
+# My program displays a plot of the functions x, x² and 2ˣ in the range [0, 4].
+# Superscrip characters copied from https://stackoverflow.com/a/51051975
+
+# x axis values 
+x = list(range(5))
+print(x)
