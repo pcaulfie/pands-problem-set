75aad6eda2c3a6186bcd1dfe59b842d3f7280933
PAUL CAULFIELD <SESA87346@se.com>
2019-03-17 09:16:48 +0000

    fixed error in code to calculate y2 axis values

--- displayplot.py
+++ displayplot.py
@@ -2,15 +2,23 @@
 # My program displays a plot of the functions x, x² and 2ˣ in the range [0, 4].
 # Superscrip characters copied from https://stackoverflow.com/a/51051975
 
-# x & y values 
+# Line 1 points
+# x axis values 
 x1 = list(range(5))
-y1= [x1*0 for x1 in range(5)]
+# y axis value
+y1= [i*0 for i in x1]
 
+# Line 2 points
+# x axis value
 x2 = list(range(5))
-y2= [x2**x2 for x2 in range(5)]
+# y axis value, the square of the vlues in x, adapted from https://www.quora.com/How-do-I-square-numbers-in-a-list-in-Python
+y2= [i**2 for i in x2]
 
+# Line 2 points
+# x axis values
 x3 = list(range(5))
-y3= [2*x3 for x3 in range(5)]
+# y axis value, 
+y3= [2*i for i in x3]
 print(x1)
 print(y1)
 print(x2)
