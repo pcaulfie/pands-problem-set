3a87c30f20d92dfc4357de3a2806f457e7b1f414
PAUL CAULFIELD <SESA87346@se.com>
2019-03-17 10:10:03 +0000

    Cleaned up code to remove duplicated code and replace with single statement to define x = list of values in range [0,4). Added comments to explain code and fixed error in function to define y1 as value should be same as input value and not zero as originally thought

--- displayplot.py
+++ displayplot.py
@@ -5,39 +5,46 @@
 # importing the required module - adapted program from https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
 import matplotlib.pyplot as plt 
 
-# Line 1 points
-# x axis values 
-x1 = list(range(5))
-# y axis value
-y1= [i*0 for i in x1]
+# Define the x-axis and corresponding y-axis values as lists for each line to be plotted:
+# In order to display 3 lines on same graph. We differentiate between them by giving them a name(label) 
+# 1 line for each function.
+
+# Define list called "x" containing list of numbers in range [0,4]
+x = list(range(5))
+
+# Define Line 1 points
+# x axis values containing input value of x 
+x1 = x
+# y axis value 
+y1= x
 # plotting the line 1 points  
 plt.plot(x1, y1, label = "Function x")
 
-# Line 2 points
+# Define Line 2 point
 # x axis value
-x2 = list(range(5))
+x2 = x
 # y axis value, the square of the vlues in x, adapted from https://www.quora.com/How-do-I-square-numbers-in-a-list-in-Python
-y2= [i**2 for i in x2]
+y2= [i**2 for i in x]
 # plotting the line 2 points  
 plt.plot(x2, y2, label = "Function x²") 
 
 # Line 2 points
 # x axis values
-x3 = list(range(5))
+x3 = x
 # y axis value, 
-y3= [2*i for i in x3]
+y3= [2*i for i in x]
 # plotting the line 3 points  
 plt.plot(x3, y3, label = "Function 2ˣ") 
 
-# naming the x axis 
+# naming the x axis using .xlabel() function.
 plt.xlabel('x - axis') 
-# naming the y axis 
+# naming the y axis using .ylabel() function.
 plt.ylabel('y - axis') 
-# giving a title to my graph 
+# giving a title to my plot using .title() function.
 plt.title('Plot of Functions x, x², 2ˣ') 
   
-# show a legend on the plot 
+# Display legend on the plot using .legend() function.
 plt.legend() 
   
-# function to show the plot 
+# Display the plot, using .show() function.
 plt.show()
