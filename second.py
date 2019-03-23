f5e30ffbd58ed02fc703b8a8410e90c2135898b3
PAUL CAULFIELD <SESA87346@se.com>
2019-03-23 22:12:34 +0000

    Made some corrections following testing - 16 lines input, 8 lines output. Added some comments and references

--- second.py
+++ second.py
@@ -6,19 +6,20 @@
 with open("moby-dick.txt", 'r') as f:
  # Opens text file "moby-dick.txt" saved in the pands-problem-set directory, the file is opened in read only
 # f = text which is read by the program
-
     
     count = 1
-    # Index value is set to 1
+    # Index value is set to 1 adapted from https://stackoverflow.com/a/30551984
     for line in f:
+        # defines line in f which will neet below conditions
             if not line.isspace():
-            # If line is not a blank line adapted from https://stackoverflow.com/a/2369538
+            # If line is not a blank line adapted from https://stackoverflow.com/a/2369538 
+            # isspace() checks whether the string consists of whitespace ref https://www.tutorialspoint.com/python/string_isspace.htm
+
                 count = count + 1
                 # Add 1 to index value
-                # adapted program from solution found in https://stackoverflow.com/a/44425842
-                  
+                                 
                 if count % 2 == 0:
-                # Argument to test if count of line number is divisible by 2 with no remainder
+                # Argument to test if count of line number is divisible by 2 with no remainder # adapted program from solution found in https://stackoverflow.com/a/44425842
                 # As every secound line will have a count which is an even number, it will be divisible by 2 with no remainder.
         
                     print(line)
