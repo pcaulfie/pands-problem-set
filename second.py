6ceed06efd40db699f9696a1c1047702a280188e
PAUL CAULFIELD <SESA87346@se.com>
2019-03-23 13:43:28 +0000

    Changed the code as I had read the problem wrong the first time. I adapted final solution from solution found in https://stackoverflow.com/a/44425842

--- second.py
+++ second.py
@@ -1,12 +1,17 @@
-# def second(moby-dick.txt):
-    
-with open("moby-dick.txt") as f:
-            # adapted progrm from solution found in https://stackoverflow.com/a/16922328
-        for line in f:
-        # line is used to store text extracted as f 
-            secondword = (' '.join(line.split()[::2]))
-            # splits lines of text in textfile into words using whitespace as the delimiter - reference https://www.geeksforgeeks.org/python-string-split/
-                # [::2] extracts every second word in the textfile beginning with the first word- reference https://docs.python.org/2.3/whatsnew/section-slices.html
-                # join every second word with whitespace between each word to form new list of words - adapted from https://stackoverflow.com/a/17645386
-                # Adapted code from https://stackoverflow.com/a/17645629 
-            print(secondword)
+# # Paul Caulfield, 2019
+# My program reads in a text file and outputs every second line. 
+# The program takes the filename of the textfile from an argument on the command line.
+
+# Opens filename saved in the pands-problem-set directory, the file is opened in read only
+#     
+with open("moby-dick.txt", 'r') as f:
+            # adapted program from solution found in https://stackoverflow.com/a/44425842
+    for count, line in enumerate(f, start=1):
+    # counts the number of lines in f, starting at 1
+        if count % 2 == 0:
+        # Define argument; if count of line number is divisible by 2 with no remainder
+            print(line)
+            # Print line if argument is true, if false, go to next line
+            
+            f.closed 
+            # File is now closed   
